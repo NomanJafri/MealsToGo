a2febb63c7877159e684c03c4c90102049b4654b
Noman Jafri <snomanh@hotmail.com>
2021-03-26 01:19:23 +0000

    Implement theme through styled components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,12 +3,29 @@ import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
-const Title = styled.Text`
-  padding: 16px;
-  font-size: 28px;
-  color: red;
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]}};
 `;
 
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary}};
+`;
+
+const RestauranCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]}};
+  background-color: ${(props) => props.theme.colors.bg.primary}};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Title = styled(Text)`  
+  font-family: ${(props) => props.theme.fonts.heading}
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.text.primary};
+`;
 
 export default function RestaurantInfoCard({ restaurant = {} }) {
   const {
@@ -23,20 +40,13 @@ export default function RestaurantInfoCard({ restaurant = {} }) {
 
   return (
     <>
-      <Card elevation={5} style={styles.card}>
-        <Card.Cover
-          key={name}
-          style={styles.cover}
-          source={{ uri: photos[0] }}
-        />
-        <Title>{name}</Title>
-      </Card>
+      <RestaurantCard>
+        <RestauranCardCover key={name} source={{ uri: photos[0] }} />
+        <Info>
+          <Title>{name}</Title>
+          <Address>{address}</Address>
+        </Info>
+      </RestaurantCard>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-  title: { padding: 16, fontSize: 20 },
-});
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,34 +1,35 @@
 import React from "react";
-import { View, SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import styled from "styled-components/native";
+import { SafeAreaView, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
 
 import RestaurantInfoCard from "../components/restaurant-info-card.component";
 
-const RestaurantsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
-      <Searchbar placeholder="Search" onChangeText={null} value={null} />
-    </View>
-    <View style={styles.list}>
-      <RestaurantInfoCard />
-    </View>
-  </SafeAreaView>
-);
+const StyledSafeAreaView = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "white",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "white",
-  },
-});
+const StyledSearchView = styled.View`
+  padding: ${(props) => props.theme.space[3]}};
+  background-color:  ${(props) => props.theme.colors.bg.secondary}};
+`;
+
+const StyledListView = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]}};
+  background-color: ${(props) => props.theme.colors.bg.secondary};
+`;
+
+const RestaurantsScreen = () => (
+  <StyledSafeAreaView>
+    <StyledSearchView>
+      <Searchbar placeholder="Search" onChangeText={null} value={null} />
+    </StyledSearchView>
+    <StyledListView>
+      <RestaurantInfoCard />
+    </StyledListView>
+  </StyledSafeAreaView>
+);
 
 export default RestaurantsScreen;
